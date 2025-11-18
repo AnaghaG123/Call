d103f8627dfa5c95814060d96ac82027adb27279
ANAGHAG123 <anaghagaryam@gmail.com>
2025-11-19 00:46:18 +0530

    CL2

--- src/Services/allApi.js
+++ src/Services/allApi.js
@@ -3,21 +3,21 @@ import { BaseUrl } from "./baseURL";
 
 export const createContact = async (reqBody) => {
 
-    return axiosConfig('Post',"http://localhost:3000/contact",reqBody)
+    return axiosConfig('Post',`${BaseUrl}/contact`,reqBody)
 
 }
 export const getContact = async () => {
 
-    return axiosConfig('Get',"http://localhost:3000/contact","")
+    return axiosConfig('Get',`${BaseUrl}/contact`,"")
 
 }
 export const deleteContact = async (id) => {
 
-    return axiosConfig('Delete',`http://localhost:3000/contact/${id}`,{})
+    return axiosConfig('Delete',`${BaseUrl}/contact/${id}`,{})
 
 }
 export const editContact = async (id,reqBody) => {
 
-    return axiosConfig('Put',`http://localhost:3000/contact/${id}`,reqBody)
+    return axiosConfig('Put',`${BaseUrl}/contact/${id}`,reqBody)
 
 }
